319e2d6e95a21e3775954938644bbafd5e5ce36c
MHarmony <mharmony@mharmony.io>
2022-07-24 21:08:27 -0400

    feat(api): setup docs, middlewares, tls
    setup fastify middlewares, nestjs middlewares, swagger docs, and tls

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,4 +1,33 @@
-import { Module } from '@nestjs/common';
+import { ClassSerializerInterceptor, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import * as Joi from 'joi';
 
-@Module({})
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development', '.env.test', '.env.production'],
+      cache: true,
+      isGlobal: true,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'test', 'production')
+          .default('development')
+          .required(),
+        PORT: Joi.number().default(3000).required(),
+        SESSION_SECRET: Joi.string().length(32).required(),
+        SESSION_SALT: Joi.string().length(16).required()
+      }),
+      validationOptions: {
+        abortEarly: true
+      }
+    })
+  ],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ClassSerializerInterceptor
+    }
+  ]
+})
 export class AppModule {}
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,22 +1,94 @@
-import { VersioningType } from '@nestjs/common';
+import fastifyCompress from '@fastify/compress';
+import fastifyCors from '@fastify/cors';
+import fastifyCsrf from '@fastify/csrf-protection';
+import fastifyHelmet from '@fastify/helmet';
+import fastifySecureSession from '@fastify/secure-session';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
 
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter()
+    new FastifyAdapter({
+      http2: true,
+      https: {
+        ca: readFileSync(resolve(__dirname, '../../../certs/ca.crt')),
+        cert: readFileSync(resolve(__dirname, '../../../certs/api.crt')),
+        key: readFileSync(resolve(__dirname, '../../../certs/api.key')),
+        dhparam: readFileSync(resolve(__dirname, '../../../certs/api.dhparam')),
+        minVersion: 'TLSv1.2',
+        ecdhCurve: 'prime256v1',
+        honorCipherOrder: true
+      }
+    })
   );
+  const configService = app.get(ConfigService);
 
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      disableErrorMessages: configService.get<string>('NODE_ENV') === 'production',
+      whitelist: true
+    })
+  );
   app.enableVersioning({
     type: VersioningType.URI,
     defaultVersion: '1'
   });
 
-  await app.listen(environment.port);
+  app.register(fastifyHelmet, {
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: [`'self'`],
+        styleSrc: [`'self'`, `'unsafe-inline'`],
+        imgSrc: [`'self'`, 'data:', 'validator.swagger.io'],
+        scriptSrc: [`'self'`, `https: 'unsafe-inline'`]
+      }
+    }
+  });
+  await app.register(fastifyCors, {
+    origin: '*',
+    credentials: true
+  });
+  await app.register(fastifySecureSession, {
+    cookie: {
+      httpOnly: true,
+      secure: true,
+      signed: true
+    },
+    secret: configService.get<string>('SESSION_SECRET'),
+    salt: configService.get<string>('SESSION_SALT')
+  });
+  await app.register(fastifyCsrf, {
+    sessionPlugin: '@fastify/secure-session'
+  });
+  await app.register(fastifyCompress);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('pagerr API')
+    .setDescription('API for the pagerr book tracking application')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('api', app, document, {
+    customSiteTitle: 'pagerr - API Documentation',
+    swaggerOptions: {
+      deepLinking: true,
+      filter: true,
+      tagsSorter: 'alpha',
+      operationsSorter: 'alpha'
+    }
+  });
+
+  await app.listen(configService.get<number>('PORT'));
 }
 
 bootstrap();
